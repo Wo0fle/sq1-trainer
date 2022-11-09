b2d7c4c0c31ee216d7d8b2884d8d708a35388704
Seby Amador <81187812+Wo0fle@users.noreply.github.com>
2022-11-09 10:56:51 -0500

    not using haxe anymore
    making a website instead

--- source/Squan.hx
+++
@@ -1,1 +0,0 @@
-/** This is where the virtual Square-1 will be born **/
--- source/Trainer.hx
+++
@@ -1,4 +0,0 @@
-/**
-	This will be a general Trainer object that will have specific cases
-	to access for depending on which trainer the user wants
-**/

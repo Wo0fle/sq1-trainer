746e3543ce01133b7a7960231d8870df868a8291
Seby Amador <81187812+Wo0fle@users.noreply.github.com>
2022-11-11 09:15:47 -0500

    AHHHHHHHH

--- source/js/squan-logic/layer.js
+++ source/js/squan-logic/layer.js
@@ -1,19 +1,29 @@
 // This will be where the logic for each virtual Square-1 layer will be
 
-export class Layer { 
-    constructor(type) {
-        this.type = type;
+const pieces = [2, 1, 2, 1, 2, 1, 2, 1]; // order of corners (2) and edges (1) in cubeshape as the starting position
 
-        const pieces = [2, 1, 2, 1, 2, 1, 2, 1];
+export class Layer { 
+    constructor(type=String) {
+        this.type = type;
     }
     turnClockwise() {
         if (this.type == "top") {
-            let lastPiece = pieces.shift();
-            pieces.push(lastPiece)
-            // idk how js works
+            let lastPiece = pieces.pop(); // take out last piece
+            pieces.unshift(lastPiece); // put it in the first spot
+        }
+        if (this.type == "bottom") {
+            let firstPiece = pieces.shift(); // take out first piece
+            pieces.push(firstPiece); // put it in the last spot
         }
     }
     turnCounterclockwise() {
-        console.log("U'/D'");
+        if (this.type == "top") {
+            let firstPiece = pieces.shift(); // take out first piece
+            pieces.push(firstPiece); // put it in the last spot
+        }
+        if (this.type == "bottom") {
+            let lastPiece = pieces.pop(); // take out last piece
+            pieces.unshift(lastPiece); // put it in the first spot
+        }
     }
 }

b17dabbd10d0f6b0e44e93d07a11635a1731d34e
Seby Amador <officiallywoofle@gmail.com>
2022-11-24 16:26:53 -0500

    js is hard :(

--- source/scripts/pick-cases.js
+++ source/scripts/pick-cases.js
@@ -3,5 +3,18 @@ function selectSet() {
     let currentSet = caseSet.options[caseSet.selectedIndex].text;  
 
     document.getElementById("curSet").innerHTML = currentSet;
-    console.log(currentSet); // pick cases somehow
+    console.log(currentSet);
+
+    retrieveCases(currentSet);
+}
+
+function retrieveCases(curSet = 'ERROR') {
+    // return possible cases (from txt file) from current case set
+    let currentSet = curSet.toLowerCase();
+
+    selectCases(currentSet);
+}
+
+function selectCases(curSet) {
+    // select from the possible cases which should be in checkboxes
 }

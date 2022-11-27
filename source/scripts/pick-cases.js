308c9a49a87f52f0655bf2f990a6daf8a83a9a91
Seby Amador <officiallywoofle@gmail.com>
2022-11-26 19:45:00 -0500

    cases show up
    now i gotta remove em lol

--- source/scripts/pick-cases.js
+++ source/scripts/pick-cases.js
@@ -26,8 +26,52 @@ async function getCases(currentSet = 'ERROR') {
     pickCases();
 }
 
+/*function removeOldCases() {
+    // remove the old cases
+    // use Element.remove()
+
+    // prob gotta loop through all cases in current case set, and remove em
+}*/
+
 function createCases() {
-    //add cases to html
+    const caseContainer = document.getElementById("case-container")
+    const casePlace = document.getElementById("casesGoHere");
+
+    for (let curCase in cases.evenCases) {
+        let caseName = cases.evenCases[curCase][0]
+
+        const newCaseDiv = document.createElement("div");
+        newCaseDiv.setAttribute("class", "case-checkbox")
+        newCaseDiv.setAttribute("value", caseName)
+        const newCaseContent = document.createTextNode(caseName);
+
+        const checkbox = document.createElement("input");
+        checkbox.setAttribute("type", "checkbox");
+        checkbox.setAttribute("value", caseName)
+
+        newCaseDiv.appendChild(checkbox);
+        newCaseDiv.appendChild(newCaseContent);
+
+        caseContainer.insertBefore(newCaseDiv, casePlace);
+    } //create even parity cases
+
+    for (let curCase in cases.oddCases) {
+        let caseName = cases.oddCases[curCase][0]
+
+        const newCaseDiv = document.createElement("div");
+        newCaseDiv.setAttribute("class", "case-checkbox")
+        newCaseDiv.setAttribute("value", caseName)
+        const newCaseContent = document.createTextNode(caseName);
+
+        const checkbox = document.createElement("input");
+        checkbox.setAttribute("type", "checkbox");
+        checkbox.setAttribute("value", caseName)
+
+        newCaseDiv.appendChild(checkbox);
+        newCaseDiv.appendChild(newCaseContent);
+
+        caseContainer.insertBefore(newCaseDiv, casePlace);
+    } //create odd parity cases
 }
 
 function pickCases() {

99955ff57b9d6be21a52d63a5696eb18ab0feb44
Seby Amador <81187812+Wo0fle@users.noreply.github.com>
2022-11-30 16:15:15 -0500

    Update df + cp.js

--- source/scripts/case-sets/df + cp.js
+++ source/scripts/case-sets/df + cp.js
@@ -5,8 +5,8 @@ const cases = {
         ["Front Headlights", "(4,-3) / (-3,0) / (-1,2) / (1,-2) / (-3,3) / (-3,0) /"],
         ["Back Headlights", "(0,-1) / (4,-2) / (-3,0) / (0,3) / (0,-3) / (-1,2) /"],
         ["Opposite Corner Swap", "(1,0) / (-4,-3) / (-3,0) / (-3,-3) / (-3,0) / (-2,-3) /"],
-        ["No Corner Swap", "(1,0) / (3,0) / (3,0) / (-1,-1) / (-2,1) / (-3,0) /"]
+        ["No Corner Swap", "(1,0) / (0,3) / (3,0) / (-1,-1) / (-2,1) / (0,-3) /"]
     ]
-} // from https://www.youtube.com/watch?v=OLrFbXhIyj8
+} // from https://www.youtube.com/watch?v=OLrFbXhIyj8 and https://www.sonyc-cuber.com/tutorials/PLL+1.html
 
 export {cases};
